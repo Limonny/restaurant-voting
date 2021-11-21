164af31ebb0f22970265c30ef35196a114fc8cdc
Limonny <limonny7@gmail.com>
2021-11-22 01:28:44 +0300

    repositories added

---
+++ src/main/java/com/example/restaurantvoting/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.example.restaurantvoting.repository;
+
+import com.example.restaurantvoting.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
